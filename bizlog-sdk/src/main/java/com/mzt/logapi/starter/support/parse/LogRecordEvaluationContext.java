7074f6524cc550d1f622a0a3e1df8a59b9b243db
牟站通 <muzhantong@meituan.com>
2021-03-02 20:01:05 +0800

    增加context的变量支持

--- bizlog-sdk/src/main/java/com/mzt/logapi/starter/support/parse/LogRecordEvaluationContext.java
+++ bizlog-sdk/src/main/java/com/mzt/logapi/starter/support/parse/LogRecordEvaluationContext.java
@@ -1,9 +1,11 @@
 package com.mzt.logapi.starter.support.parse;
 
+import com.mzt.logapi.context.LogRecordContext;
 import org.springframework.context.expression.MethodBasedEvaluationContext;
 import org.springframework.core.ParameterNameDiscoverer;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * DATE 4:12 PM
@@ -15,6 +17,12 @@ public class LogRecordEvaluationContext extends MethodBasedEvaluationContext {
     public LogRecordEvaluationContext(Object rootObject, Method method, Object[] arguments,
                                       ParameterNameDiscoverer parameterNameDiscoverer, Object ret, String errorMsg) {
         super(rootObject, method, arguments, parameterNameDiscoverer);
+        Map<String, Object> variables = LogRecordContext.getVariables();
+        if (variables != null && variables.size() > 0) {
+            for (Map.Entry<String, Object> entry : variables.entrySet()) {
+                setVariable(entry.getKey(), entry.getValue());
+            }
+        }
         setVariable("_ret", ret);
         setVariable("_errorMsg", errorMsg);
     }
