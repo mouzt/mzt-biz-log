cabc747eb7d8bef1ac91ca535dd0d80287942ed4
muzhantong <muzhantong@meituan.com>
2022-08-09 00:15:22 +0800

    升级

--- bizlog-sdk/src/main/java/com/mzt/logapi/starter/annotation/LogRecord.java
+++ bizlog-sdk/src/main/java/com/mzt/logapi/starter/annotation/LogRecord.java
@@ -12,39 +12,42 @@ import java.lang.annotation.*;
 @Documented
 public @interface LogRecord {
     /**
-     * 方法执行成功后的日志模版
+     * @return 方法执行成功后的日志模版
      */
     String success();
 
     /**
-     * 方法执行失败后的日志模版
+     * @return 方法执行失败后的日志模版
      */
     String fail() default "";
 
     /**
-     * 日志的操作人
+     * @return 日志的操作人
      */
     String operator() default "";
 
     /**
-     * 操作日志的类型，比如：订单类型、商品类型
+     * @return 操作日志的类型，比如：订单类型、商品类型
      */
     String type();
 
     /**
-     * 日志的子类型，比如订单的C端日志，和订单的B端日志，type都是订单类型，但是子类型不一样
+     * @return 日志的子类型，比如订单的C端日志，和订单的B端日志，type都是订单类型，但是子类型不一样
      */
     String subType() default "";
 
     /**
-     * 日志绑定的业务标识
+     * @return 日志绑定的业务标识
      */
     String bizNo();
 
     /**
-     * 日志的额外信息
+     * @return 日志的额外信息
      */
     String extra() default "";
 
+    /**
+     * @return 是否记录日志
+     */
     String condition() default "";
 }
